11c42222fec0ef7de9042cfc36529905c6b3e970
Adnan <adnanali89000@gmail.com>
2025-02-17 13:53:56 +0500

    Worked on Circuit Breaker and Fault Tolerence in Microservices

--- User/src/main/java/com/microservices/User/controller/UserController.java
+++ User/src/main/java/com/microservices/User/controller/UserController.java
@@ -4,6 +4,7 @@ import com.microservices.User.entity.User;
 import com.microservices.User.exceptions.UserNotFoundException;
 import com.microservices.User.service.IUserService;
 import com.microservices.User.dto.UserDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -55,9 +56,21 @@ public class UserController {
     }
 
     @GetMapping("/rating/{id}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback" ) //fallBackMethod property is used to call that method when the current service is down
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserById(id));
     }
+
+    //creating fall back method for circuit breakers
+    public ResponseEntity<User> ratingHotelFallback(Long id, Exception exception){
+        System.out.println("Fallback executed due to exception: " + exception.getMessage());
+        User user = User.builder().name("adnan").
+                email("abc@gmail.com").
+                address("karachi").
+                id(3L).build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+    //creating fall back method for circuit breakers
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
--- User/src/main/java/com/microservices/User/entity/User.java
+++ User/src/main/java/com/microservices/User/entity/User.java
@@ -1,10 +1,7 @@
 package com.microservices.User.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.id.factory.internal.AutoGenerationTypeStrategy;
 
 import java.util.ArrayList;
@@ -15,6 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @Table(name = "Users")
 public class User {
 
